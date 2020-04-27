3bcfbf7389ae0d31cd0961f4301b577e0fff5df0
Quent1nl <56486588+Quent1nl@users.noreply.github.com>
2020-04-27 17:25:18 +0200

    Creation des components Burgers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { ApiModule } from './swagger/api.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BurgerComponent } from './burger/burger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BurgerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ApiModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
